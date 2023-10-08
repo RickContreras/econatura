62b3299790ab7012bd45af4d57d0ab37f042ef81
carlosforeroudea <carlos.forero@udea.edu.co>
2023-10-07 19:58:44 -0500

    Opciones basicas Menu

---
+++ src/main/java/udea/tecnicas/view/MenuController.java
@@ -0,0 +1,44 @@
+package udea.tecnicas.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.Group;
+
+public class MenuController {
+
+    @FXML
+    private Rectangle RRequest;
+    @FXML
+    private Group GRequest;
+    @FXML
+    private Rectangle RInform;
+    @FXML
+    private Group GInform;
+    @FXML
+    private Rectangle RCash;
+    @FXML
+    private Group GCash;
+    @FXML
+    private Rectangle RSettings;
+    @FXML
+    private Group GSettings;
+
+    @FXML
+    protected void GRequest_Click(MouseEvent e){
+
+    }
+    @FXML
+    protected void GInform_Click(MouseEvent e){
+
+    }
+    @FXML
+    protected void GCash_Click(MouseEvent e){
+
+    }
+    @FXML
+    protected void GSettings_Click(MouseEvent e){
+
+    }
+
+}
