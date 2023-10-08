baa434fcceed698e546c126630ecb7e9a82fddb1
carlosforeroudea <carlos.forero@udea.edu.co>
2023-10-08 15:39:39 -0500

    Opciones basicas Menu

--- src/main/java/udea/tecnicas/view/MenuController.java
+++ src/main/java/udea/tecnicas/view/MenuController.java
@@ -6,17 +6,36 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.Group;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.lang.runtime.SwitchBootstraps;
 import java.security.spec.RSAKeyGenParameterSpec;
+import java.security.spec.RSAOtherPrimeInfo;
 
 public class MenuController {
+
+    /*
+    private int [] activeOption;
+    private int [] olderOption;
+
+    public void MenuController(){
+        activeOption = new int[2];
+        activeOption[0]=1;
+        activeOption[1]=1;
+        olderOption = new int[2];
+        olderOption[0]=1;
+        olderOption[1]=1;
+    }*/
     protected enum Menu_Option{
-        Request,Request_Request,Request_License,Inform,Inform_Inform1,Cash,Cash_Cash,Setting_Type,Setting_Resuorce,Setting_Person
+        Request,Request_Request,Request_License,Inform,Inform_Inform_1,Cash,Cash_Cash,Settings,Settings_Type,Settings_Resource,Settings_Person
     }
 
     //<editor-fold desd="Inicializacion de objetos fxml">
+    @FXML
+    private Pane Panel1;
+    @FXML
+    private Pane Panel2;
     @FXML
     private Rectangle RRequest;
     @FXML
@@ -56,7 +75,7 @@ public class MenuController {
     @FXML
     private Rectangle RSettings_Resource;
     @FXML
-    private Group GSettings_Resurce;
+    private Group GSettings_Resource;
     @FXML
     private Rectangle RSettings_Person;
     @FXML
@@ -70,7 +89,7 @@ public class MenuController {
             Group GroupClicked = (Group)e.getSource();
 
             System.out.println(GroupClicked.getId());
-            Update_Menu(Menu_Option.Request);
+            Update_Menu(Menu_Option.valueOf(GroupClicked.getId().substring(1)));
         }
         catch (Exception Ex){
             System.out.println(Ex.getMessage());
@@ -83,8 +102,89 @@ public class MenuController {
         switch(option){
             case Request: case Request_Request :
                 RRequest.setFill(Color.web("#EEF1F4",1.0));
+                RRequest_Request.setFill(Color.web("#EEF1F4",1.0));
+                GRequest_Request.setVisible(true);
+                RRequest_License.setFill(Color.web("#FFFFFF",0.0));
+                GRequest_License.setVisible(true);
+                RInform.setFill(Color.web("#FFFFFF",0));
+                RInform_Inform_1.setFill(Color.web("#FFFFFF",0));
+                GInform_Inform_1.setVisible(false);
+                RCash.setFill(Color.web("#FFFFFF",0));
+                RCash_Cash.setFill(Color.web("#FFFFFF",0));
+                GCash_Cash.setVisible(false);
+                RSettings.setFill(Color.web("#FFFFFF",0));
+                RSettings_Person.setFill(Color.web("#FFFFFF",0));
+                GSettings_Person.setVisible(false);
+                RSettings_Resource.setFill(Color.web("#FFFFFF",0));
+                GSettings_Resource.setVisible(false);
+                RSettings_Type.setFill(Color.web("#FFFFFF",0));
+                GSettings_Type.setVisible(false);
+                break;
+            case Request_License:
+                RRequest.setFill(Color.web("#EEF1F4",1.0));
+                RRequest_Request.setFill(Color.web("#FFFFFF",0));
+                RRequest_License.setFill(Color.web("#EEF1F4",1));
+                break;
+            case Inform: case Inform_Inform_1:
+                RRequest.setFill(Color.web("#FFFFFF",0));
+                GRequest_Request.setVisible(false);
+                GRequest_License.setVisible(false);
+                RInform.setFill(Color.web("#EEF1F4",1));
+                RInform_Inform_1.setFill(Color.web("#EEF1F4",1));
+                GInform_Inform_1.setVisible(true);
+                RCash.setFill(Color.web("#FFFFFF",0));
+                GCash_Cash.setVisible(false);
+                RSettings.setFill(Color.web("#FFFFFF",0));
+                GSettings_Person.setVisible(false);
+                GSettings_Resource.setVisible(false);
+                GSettings_Type.setVisible(false);
+                break;
+            case Cash: case Cash_Cash:
+                RRequest.setFill(Color.web("#FFFFFF",0));
+                GRequest_Request.setVisible(false);
+                GRequest_License.setVisible(false);
+                RInform.setFill(Color.web("#FFFFFF",0));
+                GInform_Inform_1.setVisible(false);
+                RCash.setFill(Color.web("#EEF1F4",1));
+                RCash_Cash.setFill(Color.web("#EEF1F4",1));
+                GCash_Cash.setVisible(true);
+                RSettings.setFill(Color.web("#FFFFFF",0));
+                GSettings_Person.setVisible(false);
+                GSettings_Resource.setVisible(false);
+                GSettings_Type.setVisible(false);
+                break;
+            case Settings: case Settings_Type:
+                RRequest.setFill(Color.web("#FFFFFF",0));
+                GRequest_Request.setVisible(false);
+                GRequest_License.setVisible(false);
+                RInform.setFill(Color.web("#FFFFFF",0));
+                GInform_Inform_1.setVisible(false);
+                RCash.setFill(Color.web("#FFFFFF",0));
+                GCash_Cash.setVisible(false);
+                RSettings.setFill(Color.web("#EEF1F4",1));
+                RSettings_Person.setFill(Color.web("#FFFFFF",0));
+                GSettings_Person.setVisible(true);
+                RSettings_Resource.setFill(Color.web("#FFFFFF",0));
+                GSettings_Resource.setVisible(true);
+                RSettings_Type.setFill(Color.web("#EEF1F4",1));
+                GSettings_Type.setVisible(true);
+                break;
+            case Settings_Resource:
+                RSettings_Person.setFill(Color.web("#FFFFFF",0));
+                RSettings_Resource.setFill(Color.web("#EEF1F4",1));
+                RSettings_Type.setFill(Color.web("#FFFFFF",0));
+                break;
+            case Settings_Person:
+                RSettings_Person.setFill(Color.web("#EEF1F4",1));
+                RSettings_Resource.setFill(Color.web("#FFFFFF",0));
+                RSettings_Type.setFill(Color.web("#FFFFFF",0));
+                break;
+
 
         }
+
+    }
+    protected void Fill_Active_Option(){
 
     }
 
