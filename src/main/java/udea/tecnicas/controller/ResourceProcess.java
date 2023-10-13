75133df40880d9c18308d844d5615f5f32750c78
carlosforeroudea <carlos.forero@udea.edu.co>
2023-10-12 20:27:35 -0500

    Generacion de controladores

--- src/main/java/udea/tecnicas/controller/ResourceProcess.java
+++ src/main/java/udea/tecnicas/controller/ResourceProcess.java
@@ -1,2 +1,10 @@
-package udea.tecnicas.controller;public class ResourceProcess {
+package udea.tecnicas.controller;
+
+import udea.tecnicas.model.License;
+import udea.tecnicas.model.PenaltyFee;
+
+public class ResourceProcess {
+    public static PenaltyFee ApplyPenaltyFee(License l,String r,int c){
+        return new PenaltyFee();
+    }
 }
