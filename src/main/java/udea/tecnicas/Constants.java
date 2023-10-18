be30acbf8d76e76f429f2dfbf6bd0390d2ab2ee3
xaraxx <sara.galvan@udea.edu.co>
2023-10-18 00:35:34 -0500

    Correcciones en migraciones

--- src/main/java/udea/tecnicas/Constants.java
+++ src/main/java/udea/tecnicas/Constants.java
@@ -2,4 +2,5 @@ package udea.tecnicas;
 
 public class Constants {
     public final static String DATABASE_URL = "jdbc:sqlite:Hecatombe.db";
+    public final static int SQLITE_CONSTRAIN_ERROR = 19;
 }
